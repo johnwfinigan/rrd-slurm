453a92fbf2facdb8d01daa0dc4cce1f9111a3cff
John Finigan <john.finigan@ucdenver.edu>
2016-09-20 16:26:30 -0600

    handle multiple partitions

--- rrd_generate_graphs.sh
+++ rrd_generate_graphs.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-RRDFILE_PATH='./squeue_job_status.rrd'
+RRDFILENAME_TEMPLATE="squeue_job_status_"
+IMGTYPE="PNG"
+
+for PARTITION in `sinfo -h -o "%R"`; do
+    OUTFILE=$RRDFILENAME_TEMPLATE$PARTITION
 
 #One Day 
-rrdtool graph squeue_job_status_daily.png -a PNG --title="Daily Job Status" --vertical-label "#Jobs" 'DEF:probe1=squeue_job_status.rrd:Running:AVERAGE:step=1' 'DEF:probe2=squeue_job_status.rrd:Pending:AVERAGE:step=1' 'LINE1:probe1#00ff00:Running' 'LINE1:probe2#0400ff:Pending'
-
-
-#5 Days
-rrdtool graph squeue_job_status_5days.png -a PNG --start N-5D --end N --title="5 Day Job Status" --vertical-label "Jobs" 'DEF:probe1=squeue_job_status.rrd:Running:AVERAGE:step=10' 'DEF:probe2=squeue_job_status.rrd:Pending:AVERAGE:step=10' 'LINE1:probe1#00ff00:Running' 'LINE1:probe2#0400ff:Pending'
-
-#3 Months
-rrdtool graph squeue_job_status_3month.png -a PNG --start N-93D --end N --title="3 Month Job Status" --vertical-label "Jobs" 'DEF:probe1=squeue_job_status.rrd:Running:AVERAGE:step=2880' 'DEF:probe2=squeue_job_status.rrd:Pending:AVERAGE:step=2880' 'DEF:probe3=squeue_job_status.rrd:Running:MAX:step=2880' 'DEF:probe4=squeue_job_status.rrd:Pending:MAX:step=2880' 'LINE1:probe1#00ff00:Running Average' 'LINE1:probe2#0400ff:Pending Average' 'LINE1:probe4#00ffff:Pending Max' 'LINE1:probe3#f0ff00:Running Max'
+    rrdtool graph $OUTFILE"_daily."$IMGTYPE -a $IMGTYPE --title="$PARTITION Daily Queue Depth" --vertical-label "#Jobs" "DEF:probe1=$OUTFILE.rrd:Running:AVERAGE:step=1" "DEF:probe2=$OUTFILE.rrd:Pending:AVERAGE:step=1" "LINE1:probe1#00ff00:Running" "LINE1:probe2#0400ff:Pending"
+    
+    
+    #5 Days
+    rrdtool graph $OUTFILE"_5days."$IMGTYPE -a $IMGTYPE  --start N-5D --end N --title="$PARTITION 5 Day Queue Depth" --vertical-label "Jobs" "DEF:probe1=$OUTFILE.rrd:Running:AVERAGE:step=10" "DEF:probe2=$OUTFILE.rrd:Pending:AVERAGE:step=10" "LINE1:probe1#00ff00:Running" "LINE1:probe2#0400ff:Pending"
+    
+    #3 Months
+    rrdtool graph $OUTFILE"_3months."$IMGTYPE -a $IMGTYPE --start N-93D --end N --title="$PARTITION 3 Month Queue Depth" --vertical-label "Jobs" "DEF:probe1=$OUTFILE.rrd:Running:AVERAGE:step=2880" "DEF:probe2=$OUTFILE.rrd:Pending:AVERAGE:step=2880" "DEF:probe3=$OUTFILE.rrd:Running:MAX:step=2880" "DEF:probe4=$OUTFILE.rrd:Pending:MAX:step=2880" "LINE1:probe1#00ff00:Running Average" "LINE1:probe2#0400ff:Pending Average" "LINE1:probe4#00ffff:Pending Max" "LINE1:probe3#f0ff00:Running Max"
+done
