fc0971338d062d4cf223a58f66b4b9816be535cb
John Finigan <johnwfinigan@gmail.com>
2016-09-20 10:04:55 -0600

    remove hidden sudo, change rrd file to be created w relative path, remove unneeded exports

--- rrd_create_squeue_status.sh
+++ rrd_create_squeue_status.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 #Creates the squeue_job_status rrd
-sudo rrdtool create /var/log/squeue_job_status.rrd --start N-93D --step 30 DS:Running:GAUGE:600:0:U DS:Pending:GAUGE:600:0:U RRA:AVERAGE:0.5:10:1440 RRA:AVERAGE:0.5:1:2880 RRA:AVERAGE:0.5:2880:93 RRA:MAX:0.5:2880:93
 
-#rrdtool create /var/log/squeue_job_status.rrd --start N-93D --step 30  #30 second steps, should be <= update frequency
+RRDFILE_PATH='./squeue_job_status.rrd'
+
+rrdtool create $RRDFILE_PATH --start N-93D --step 30 DS:Running:GAUGE:600:0:U DS:Pending:GAUGE:600:0:U RRA:AVERAGE:0.5:10:1440 RRA:AVERAGE:0.5:1:2880 RRA:AVERAGE:0.5:2880:93 RRA:MAX:0.5:2880:93
+
+#rrdtool create ./squeue_job_status.rrd --start N-93D --step 30  #30 second steps, should be <= update frequency
 #DS:Running:GAUGE:600:0:U DS:Pending:GAUGE:600:0:U                      #Creates the guages for running and pending programs
 #                                                                       ##with a 600 second heartbeat: time before assuming unkown
 #RRA:AVERAGE:0.5:10:1440   #5 days, 5 min steps 
--- rrd_update_squeue_status.sh
+++ rrd_update_squeue_status.sh
@@ -2,8 +2,8 @@
 
 #Updates the rrd, should be run with t <= highest frequency stored.
 
-export path='/var/log/squeue_job_status.rrd'                                 #path to the rrd file
-export running=$(squeue | awk '$5~/R/ {print $5}' | wc | awk '{print $1}')   ##processes running
-export pending=$(squeue | awk '$5~/Pd?/ {print $5}' | wc | awk '{print $1}') ##processes pending
+RRDFILE_PATH='./squeue_job_status.rrd'                                 #path to the rrd file
+running=$(squeue | awk '$5~/R/ {print $5}' | wc | awk '{print $1}')   ##processes running
+pending=$(squeue | awk '$5~/Pd?/ {print $5}' | wc | awk '{print $1}') ##processes pending
 
-sudo rdtool update $path "N:$running:$pending"
+rdtool update $RRDFILE_PATH "N:$running:$pending"
