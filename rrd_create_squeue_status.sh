301dcf216ebefdbb6c97514593235d66cc32afdb
John Finigan <john.finigan@ucdenver.edu>
2016-09-20 10:57:37 -0600

    check rrd file existance, create only if not exist

--- rrd_create_squeue_status.sh
+++ rrd_create_squeue_status.sh
@@ -3,7 +3,9 @@
 
 RRDFILE_PATH='./squeue_job_status.rrd'
 
-rrdtool create $RRDFILE_PATH --start N-93D --step 30 DS:Running:GAUGE:600:0:U DS:Pending:GAUGE:600:0:U RRA:AVERAGE:0.5:10:1440 RRA:AVERAGE:0.5:1:2880 RRA:AVERAGE:0.5:2880:93 RRA:MAX:0.5:2880:93
+if [ ! -f $RRDFILE_PATH ] ; then
+    rrdtool create $RRDFILE_PATH --start N-93D --step 30 DS:Running:GAUGE:600:0:U DS:Pending:GAUGE:600:0:U RRA:AVERAGE:0.5:10:1440 RRA:AVERAGE:0.5:1:2880 RRA:AVERAGE:0.5:2880:93 RRA:MAX:0.5:2880:93
+fi
 
 #rrdtool create ./squeue_job_status.rrd --start N-93D --step 30  #30 second steps, should be <= update frequency
 #DS:Running:GAUGE:600:0:U DS:Pending:GAUGE:600:0:U                      #Creates the guages for running and pending programs
