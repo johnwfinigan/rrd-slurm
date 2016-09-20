dea0a37693ef02b6864c7d30b511225b4362a9eb
John Finigan <john.finigan@ucdenver.edu>
2016-09-20 11:26:08 -0600

    fix typo rdtool -> rrdtool

--- rrd_update_squeue_status.sh
+++ rrd_update_squeue_status.sh
@@ -6,4 +6,4 @@ RRDFILE_PATH='./squeue_job_status.rrd'                                 #path to 
 running=$(squeue | awk '$5~/R/ {print $5}' | wc | awk '{print $1}')   ##processes running
 pending=$(squeue | awk '$5~/Pd?/ {print $5}' | wc | awk '{print $1}') ##processes pending
 
-rdtool update $RRDFILE_PATH "N:$running:$pending"
+rrdtool update $RRDFILE_PATH "N:$running:$pending"
