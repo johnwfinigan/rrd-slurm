0a2dc7563aae181cfa8aca42d88a9c5382f0e3f9
Jacob Thompson <jacthomp@mymail.mines.edu>
2016-10-04 11:02:58 -0600

    Updated the times in the update script RRAs, added a 2 year graph to generate graphs

--- rrd_generate_graphs.sh
+++ rrd_generate_graphs.sh
@@ -7,18 +7,22 @@ for PARTITION in `sinfo -h -o "%R"`; do
     OUTFILE=$RRDFILENAME_TEMPLATE$PARTITION
 
 #One Day 
-    rrdtool graph $OUTFILE"_daily."$IMGTYPE -a $IMGTYPE --title="$PARTITION Daily Queue Depth" \
+    rrdtool graph $OUTFILE"_daily."$IMGTYPE -a $IMGTYPE --start N-1D --end N --title="$PARTITION Daily Queue Depth" \
     --vertical-label "#Jobs" "DEF:probe1=$OUTFILE.rrd:Running:AVERAGE" \
     "DEF:probe2=$OUTFILE.rrd:Pending:AVERAGE" "LINE1:probe1#00ff00:Running" "LINE2:probe2#0400ff:Pending"
     
-    
-    #5 Days
+#5 Days
     rrdtool graph $OUTFILE"_5days."$IMGTYPE -a $IMGTYPE  --start N-5D --end N --title="$PARTITION 5 Day Queue Depth" \
     --vertical-label "Jobs" "DEF:probe1=$OUTFILE.rrd:Running:AVERAGE" \
     "DEF:probe2=$OUTFILE.rrd:Pending:AVERAGE" "LINE1:probe1#00ff00:Running" "LINE2:probe2#0400ff:Pending"
     
-    #3 Months
+#3 Months
     rrdtool graph $OUTFILE"_3months."$IMGTYPE -a $IMGTYPE --start N-93D --end N --title="$PARTITION 3 Month Queue Depth" \
+    --vertical-label "Jobs" "DEF:probe1=$OUTFILE.rrd:Running:AVERAGE" \
+    "DEF:probe2=$OUTFILE.rrd:Pending:AVERAGE" "LINE1:probe1#00ff00:Running" "LINE2:probe2#0400ff:Pending"
+
+#2 Years
+    rrdtool graph $OUTFILE"_3months."$IMGTYPE -a $IMGTYPE --start N-2Y --end N --title="$PARTITION 2 Year Queue Depth" \
     --vertical-label "Jobs" "DEF:probe1=$OUTFILE.rrd:Running:AVERAGE" \
     "DEF:probe2=$OUTFILE.rrd:Pending:AVERAGE" "LINE1:probe1#00ff00:Running" "LINE2:probe2#0400ff:Pending"
 done
